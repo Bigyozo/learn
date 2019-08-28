afe618f114fa4789ce99adba98d397d022b59b2b
Bigyozo <502204620@qq.com>
2019-08-28 20:47:39 +0800

    add files

--- src/T2019/Test.java
+++ src/T2019/Test.java
@@ -1,5 +1,7 @@
 package T2019;
 
+import java.util.HashMap;
+
 /**
  * @Auther: zhangfanglong
  * @Date: 2019/8/25/025 19:44
@@ -10,6 +12,10 @@ public class Test {
         byte s1=127; s1=(byte)(s1 +1);
         short s2 =1; s2+=1;
         System.out.println(s1+" "+s2);
+        HashMap<Integer,Integer> map=new HashMap<>();
+        map.put(1,2);
+        map.put(1,3);
+        System.out.println(map.get(1));
     }
 
 }
